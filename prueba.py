e6862cc64167baa64db56d9c7392ab882a645a85
jazalvarez22 <jazzdimelo6@gmail.com>
2026-01-09 12:06:20 -0500

    actualizacion

---
+++ prueba.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy
+import math
+
+print("NumPy version:", np.__version__)
+print("SciPy version:", scipy.__version__)
+print("Math funciona correctamente")
+
+# Prueba simple
+print("\nPrueba de numpy:", np.array([1, 2, 3]))
